7c60ef1f86e698de09b238fe7fd421c717f8762d
dev <itinera.support@famifed.be>
2020-04-16 15:17:33 +0200

    using md5

--- app.js
+++ app.js
@@ -4,8 +4,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
-const encrypt = require("mongoose-encryption");
-
+const md5 = require("md5");
 const app = express();
 
 app.set('view engine', 'ejs');
@@ -25,9 +24,6 @@ const userSchema = new mongoose.Schema ({
   password: String
 });
 
-const secret = process.env.SECRET;
-userSchema.plugin(encrypt, {secret: secret, encryptedFields: ["password"]});
-
 const User = new mongoose.model("User", userSchema);
 
 
@@ -46,7 +42,7 @@ app.get("/register", function(req, res) {
 app.post("/register", function(req, res){
   const newUser= new User({
     email: req.body.username,
-    password: req.body.password
+    password: md5(req.body.password)
   });
 
   newUser.save(function(err){
@@ -64,7 +60,7 @@ app.post("/login", function(req, res){
       console.log(err);
     }else{
       if(foundUser){
-        if(foundUser.password === req.body.password){
+        if(foundUser.password === md5(req.body.password)){
           res.render("secrets");
         }
       }
